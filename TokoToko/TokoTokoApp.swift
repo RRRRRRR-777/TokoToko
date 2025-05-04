63dec3c19ef3e7b3dcd1a742a3b87c5530d72e36
はじめましてやまだです。 <96092291+RRRRRRR-777@users.noreply.github.com>
2025-05-04 22:00:59 +0900

    Initial Commit

---
+++ TokoToko/TokoTokoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokoTokoApp.swift
+//  TokoToko
+//
+//  Created by bokuyamada on 2025/05/04.
+//
+
+import SwiftUI
+
+@main
+struct TokoTokoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
